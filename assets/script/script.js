68e0fb2cdc79daf7ef71a54f370e998324ae91e9
jkworks <info@jilliankayworks.com>
2021-08-03 18:40:19 -0600

    updated script and local storage past buttons

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,14 +1,15 @@
 var todaysForecast = document.getElementById("todaysforecast");
 var searchBtn = document.getElementById("searchBtn");
 var cityBtn = document.getElementById("searchBtns");
+var pastCitySearch = JSON.parse(localStorage.getItem("data")) || [];
 
+//add for loop over pCS that will run in 2 places one on page load and after search city gets ran
 
-//add to local storage 
-
+//new function up here called 
 function cityButton(event){
   event.preventDefault();
   var buttonValue = event.target.textContent
-  var cityDisplay = document.getElementById("searchcity").value;
+  var cityDisplay = document.getElementById("searchcity");
 
   cityDisplay.value = buttonValue
 
@@ -29,11 +30,15 @@ function citySearch(event){
   newBtn.textContent = cityDisplay
   cityBtn.append(newBtn)
   newBtn.setAttribute("style", "display: block; width: 90%; color: whitesmoke; border: 2px lightcyan solid; border-radius: 10px; background-color: lightslategray; padding: 10px; margin: 1%;")
-
+// call here 
+pastCitySearch.push(cityDisplay)
+localStorage.setItem("data",JSON.stringify(pastCitySearch));
+//run function here 
   getWeather(event)
 }
 
 function getWeather(event) {
+  event.preventDefault();
   
   var cityDisplay = document.getElementById("searchcity").value;
   // fetch request gets a list of all the repos for the node.js organization
@@ -47,7 +52,8 @@ function getWeather(event) {
     })
     .then(function (data) {
       console.log(data);
-      localStorage.setItem("data",JSON.stringify(data[0].name));
+      //pulls city name out of object array index and sets to local storage 
+     
       return data;
     })
 
@@ -57,25 +63,67 @@ function getWeather(event) {
         .then(function (response) {
           return response.json();
         })
-        .then(function (data) {
-          console.log(data)
+        .then(function (response) {
+          todaysForecast.innerHTML= ""
+          console.log(response)
           //button click works returns data to console
             console.log(searchCity)
            // Creating elements, tablerow, tabledata, and anchor
            var tableData = document.createElement("td");
 
+           var weatherIcon = response.current.weather[0].icon;
+           var weatherImage = document.createElement("img");
 
-          
 
+           
+           weatherImage.setAttribute(
+             "src",
+             "http://openweathermap.org/img/wn/" + weatherIcon + "@2x.png"
+           );
+           todaysForecast.appendChild(weatherImage)
          
           // Setting the text of link and the href of the link
-          tableData.textContent = data.current.temp;
+          tableData.textContent = response.current.temp;
           // tableData.appendChild(link);
           todaysForecast.appendChild(tableData);
           // tableBody.appendChild(createTableRow);
-          cityDisplay.value = " "
+
+          document.getElementById("searchcity").value = ""
+          fiveDay(response)
         });
     });
+}
+
+function fiveDay (fivedaydata){
+
+  var fiveDayDiv = document.getElementById("fivedayforecast")
+  fiveDayDiv.innerHTML = ""
+
+  fivedaydata.daily.splice(1,5).forEach(forecast => {
+    var dateTime = forecast.dt * 1000
+    console.log(new Date(dateTime).toLocaleString())
+    var divEl = document.createElement("div");
+    var tempEl = document.createElement("p");
+
+    tempEl.textContent = "temp: " + forecast.temp.day 
+
+    divEl.append(tempEl)
+
+    var uviEl = document.createElement("p");
+
+    uviEl.textContent = "uvi: " + forecast.uvi
+
+    divEl.append(uviEl)
+
+
+
+
+    fiveDayDiv.append(divEl)
+ 
+  });
+
+
+
 }
 
 searchBtn.addEventListener("click", citySearch);
